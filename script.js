413907fee0c4c9fd60a799fe402f397faa2faebb
MikenasIgnas <mikenasignas@gmail.com>
2022-06-17 22:04:52 +0300

    Everything Works, but there still are bugs with alerts

--- script.js
+++ script.js
@@ -211,6 +211,14 @@ submitButtonContainer.style.textAlign = "center";
 submitButtonContainer.style.marginTop = "10px";
 submitButton.type = "submit";
 
+let radioValue;
+
+function setRadioValue(e) {
+  if (e.target.checked) {
+    radioValue = e.target.value;
+  }
+}
+
 function createRadioButton() {
   const radioButtonsContainer = document.createElement("div");
   radioButtonsContainer.classList.add("radioButton");
@@ -226,12 +234,9 @@ function createRadioButton() {
     radioInput.setAttribute("value", `TYPE ${i}gr`);
     radioInput.name = `radioLabel`;
     radioInput.setAttribute("Id", "radioInput");
-
-    radioLabel.htmlFor = `radioLabel${i}`;
-    radioLabel.setAttribute("value", `TYPE ${i}gr:`);
-    radioLabel.setAttribute("id", "radioLabel");
-
     radioLabel.innerHTML = `TYPE ${i}gr:`;
+
+    radioInput.addEventListener("click", setRadioValue);
 
     radioButtonsContainer.style.textAlign = "center";
     radioButtonsContainer.style.alignItems = "center";
@@ -239,23 +244,201 @@ function createRadioButton() {
     radioButtonsContainer.append(radioLabel, radioInput);
   }
 }
-
-const radioInput = document.getElementById("radioInput");
-const radioLabel = document.getElementById("radioLabel");
 const buttonSubmit = document.getElementById("submitButton");
 
+firstNameAlert = () => {
+  nameInput.style.border = "2px solid red";
+  nameInput.style.borderRadius = "2px ";
+  nameInput.placeholder = "REQUIERED";
+  nameInput.value = "";
+  nameInput.addEventListener("keydown", () => {
+    nameInput.style.border = "1px solid black";
+    nameInput.style.borderRadius = "2px ";
+  });
+};
+lastNameAlert = () => {
+  lastNameInput.style.border = "2px solid red";
+  lastNameInput.style.borderRadius = "2px ";
+  lastNameInput.placeholder = "REQUIERED";
+  lastNameInput.value = "";
+  lastNameInput.addEventListener("keydown", () => {
+    lastNameInput.style.border = "1px solid black";
+    lastNameInput.style.borderRadius = "2px ";
+  });
+};
+ageAlert = () => {
+  ageInput.style.border = "2px solid red";
+  ageInput.style.borderRadius = "2px ";
+  ageInput.placeholder = "REQUIERED";
+  ageInput.value = "";
+  ageInput.addEventListener("keydown", () => {
+    ageInput.style.border = "1px solid black";
+    ageInput.style.borderRadius = "2px ";
+  });
+};
+phoneAlert = () => {
+  phoneInput.style.border = "2px solid red";
+  phoneInput.style.borderRadius = "2px ";
+  phoneInput.placeholder = "REQUIERED";
+  phoneInput.value = "";
+  phoneInput.addEventListener("keydown", () => {
+    phoneInput.style.border = "1px solid black";
+    phoneInput.style.borderRadius = "2px ";
+  });
+};
+
+emailAlert = () => {
+  emailInput.style.border = "2px solid red";
+  emailInput.style.borderRadius = "2px ";
+  emailInput.placeholder = "REQUIERED";
+  emailInput.value = "";
+  emailInput.addEventListener("keydown", () => {
+    emailInput.style.border = "1px solid black";
+    emailInput.style.borderRadius = "2px ";
+  });
+};
+
 buttonSubmit.addEventListener("click", (e) => {
+  const radioInput = document.getElementById("radioInput");
   e.preventDefault();
-  emailInput.value !== "" &&
-  nameInput.value !== "" &&
-  lastNameInput.value !== "" &&
-  ageInput.value !== "" &&
-  rangeTicks.checked !== "" &&
-  radioInput.checked !== ""
-    ? alert(
-        `Name: ${nameInput.value}, Lastname: ${lastNameInput.value}, Email: ${emailInput.value}, Age: ${ageInput.value}, It knowledge: ${rangeInput.value},${radioLabel.value}`
-      )
-    : alert("You didint fill out the requiered fields");
+
+  let secretEmailSymbols = "";
+  for (let i = 0; i < emailInput.value.length; i++) {
+    secretEmailSymbols = `${secretEmailSymbols}*`;
+  }
+
+  let secretPhoneSymbols = "";
+  for (let i = 0; i < phoneInput.value.length; i++) {
+    secretPhoneSymbols = `${secretPhoneSymbols}*`;
+  }
+
+  if (nameInput.value === "") {
+    firstNameAlert();
+  }
+  if (lastNameInput.value === "") {
+    lastNameAlert();
+  }
+  if (ageInput.value === "") {
+    ageAlert();
+  }
+  if (phoneInput.value === "") {
+    phoneAlert();
+  }
+  if (emailInput.value === "") {
+    emailAlert();
+  }
+  if (
+    nameInput.value === "" &&
+    lastNameInput.value === "" &&
+    ageInput.value === "" &&
+    phoneInput.value === "" &&
+    emailInput.value === ""
+  ) {
+    alert("Required fields are not filled");
+  }
+  if (lastNameInput.value.length < 3 && nameInput.value.length < 3) {
+    alert("Name and LastName has to be at least 3 letters long");
+    lastNameAlert();
+    firstNameAlert();
+  } else if (nameInput.value.length < 3) {
+    alert("Name has to be atleast 3 letters long");
+    firstNameAlert();
+  } else if (lastNameInput.value.length < 3) {
+    alert("LastName has to be atleast 3 letters long");
+    lastNameAlert();
+  } else if (ageInput.value <= 0) {
+    alert(
+      "Age must be a positive number (Must be more than 0 and less than 120)"
+    );
+    ageAlert();
+  } else if (ageInput.value >= 120) {
+    alert("Age is invalid (Must be more than 0 and less than 120)");
+    ageAlert();
+  } else if (phoneInput.value.length < 9 || phoneInput.value.length > 12) {
+    alert("Incorect phone number (Must be between 9 and 12 numbers)");
+    phoneAlert();
+  } else if (emailInput.value.length < 5 || !emailInput.value.includes("@")) {
+    alert("Incorect Email (must contain @ and be atleast 5 symbols long)");
+    emailAlert();
+  } else {
+    const studentItem = document.createElement("div");
+    const showButton = document.createElement("button");
+
+    showButton.type = "submit";
+
+    for (let i = 0; i < 7; i++) {
+      const studentInfo = document.createElement("p");
+      studentItem.append(studentInfo);
+      studentInfo.setAttribute("value", `${i}`);
+
+      if (i === 0 && nameInput.value !== "") {
+        studentInfo.textContent += `Name: ${nameInput.value}`;
+      }
+      if (i === 1 && lastNameInput.value !== "") {
+        studentInfo.textContent += `Last name: ${lastNameInput.value}`;
+      }
+      if (i === 2 && ageInput.value !== "") {
+        studentInfo.textContent += `Age: ${ageInput.value}`;
+      }
+
+      if (i === 3 && phoneInput.value !== "") {
+        studentInfo.innerHTML += `Phone: ${secretPhoneSymbols}`;
+      }
+      if (i === 4 && emailInput.value !== "") {
+        studentInfo.textContent += `Email:${secretEmailSymbols}`;
+      }
+      if (i === 5 && rangeTicks.checked !== "") {
+        studentInfo.textContent += `It knowledge: ${rangeInput.value}`;
+      }
+      if (i === 6 && radioInput.checked !== "") {
+        studentInfo.textContent += `Group: ${radioValue}`;
+      }
+      let hiddenData = true;
+      showButton.textContent = "Show";
+      showButton.addEventListener("click", () => {
+        if (i === 3 && hiddenData) {
+          showButton.textContent = "Hide";
+          studentInfo.innerHTML = "Phone:" + phoneInput.value;
+        } else if (i === 3 && !hiddenData) {
+          showButton.textContent = "Show";
+          studentInfo.innerHTML = `Phone: ${secretPhoneSymbols}`;
+        }
+        if (i === 4 && hiddenData) {
+          showButton.textContent = "Hide";
+          studentInfo.innerHTML = "Email:" + emailInput.value;
+        } else if (i === 4 && !hiddenData) {
+          showButton.textContent = "Show";
+          studentInfo.innerHTML = `Email: ${secretEmailSymbols}`;
+        }
+        hiddenData = !hiddenData;
+      });
+    }
+
+    studentItem.style.maxWidth = "200px";
+    studentItem.style.height = "125px";
+
+    studentItem.style.margin = "auto";
+    studentItem.style.marginTop = "20px";
+    studentItem.style.border = "1px solid black";
+    studentItem.style.borderRadius = "5px ";
+    studentItem.style.padding = "8px ";
+    studentItem.style.fontSize = "7px ";
+
+    showButton.style.margin = "auto";
+    showButton.style.width = "30%";
+    showButton.style.height = "12px";
+    showButton.style.fontSize = "7px";
+    showButton.style.display = "flex";
+    showButton.style.justifyContent = "center";
+
+    container.append(studentItem);
+    studentItem.append(showButton);
+  }
+  // nameInput.value = "";
+  // lastNameInput.value = "";
+  // emailInput.value = "";
+  // ageInput.value = "";
+  // phoneInput.value = "";
 });
 
 //CTR+C from internet
